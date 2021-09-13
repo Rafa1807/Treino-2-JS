aae9c46b00f4dcb8bda988cd79971578c99d8e7d
Rafael Ribeiro Bernardes <rafaelribeiro@MacBook-Pro-de-Rafael.local>
2021-09-13 11:55:01 -0300

    Treinamento de Data curso

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+//var a = new Date()
+
+//console.log(a)
+
+// var d = new Date(2018, 08, 12, 22,45)
+
+// console.log(d)
+
+
+// var d = new Date(1000)
+
+// console.log(d)
+
+// var d = new Date("sep 05 2019 10:23")
+
+var d = new Date(1289319206239)
+
+// var dias = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"]
+
+console.log(d)
+console.log(d.getMilliseconds())
